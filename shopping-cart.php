1c51c29eafa9ae61fbb922f6cd6b2c83fdfe3bf1
Vatsal Doshi <doshivatsal7@gmail.com>
2017-08-12 09:26:08 +0530

    Checkout pages modified, button added in cart.

--- shopping-cart.php
+++ shopping-cart.php
@@ -15,7 +15,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 		function hideURLbar(){ window.scrollTo(0,1); } </script>
-
 <!--//tags -->
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 <link href="css/style-default.css" rel='stylesheet' type='text/css'/>
@@ -26,16 +25,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <!-- //for bootstrap working -->
 <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800" rel="stylesheet">
 <link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,900,900italic,700italic' rel='stylesheet' type='text/css'>
-
-<style type="text/css">
-
-</style>
-
 </head>
 
 <?php
 include('header.php');
 ?>
+
 <div class="shopping-cart-page">
 
   <div class="column-labels">
@@ -190,7 +185,6 @@ include('footer.php');
   });  
   }
 
-
   /* Remove item from cart */
   function removeItem(removeButton)
   {
@@ -202,6 +196,89 @@ include('footer.php');
   });
   }
   </script>
+  <!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<script src="js/modernizr.custom.js"></script>
+  <!-- Custom-JavaScript-File-Links --> 
+  <!-- cart-js -->
+  <script src="js/minicart.min.js"></script>
+<script>
+  // Mini Cart
+  paypal.minicart.render({
+    action: '#'
+  });
+
+  if (~window.location.search.indexOf('reset=true')) {
+    paypal.minicart.reset();
+  }
+</script>
+
+  <!-- //cart-js --> 
+<!-- script for responsive tabs -->           
+<script src="js/easy-responsive-tabs.js"></script>
+<script>
+  $(document).ready(function () {
+  $('#horizontalTab').easyResponsiveTabs({
+  type: 'default', //Types: default, vertical, accordion           
+  width: 'auto', //auto or any width like 600px
+  fit: true,   // 100% fit in a container
+  closed: 'accordion', // Start closed if in accordion view
+  activate: function(event) { // Callback function if tab is switched
+  var $tab = $(this);
+  var $info = $('#tabInfo');
+  var $name = $('span', $info);
+  $name.text($tab.text());
+  $info.show();
+  }
+  });
+  $('#verticalTab').easyResponsiveTabs({
+  type: 'vertical',
+  width: 'auto',
+  fit: true
+  });
+  });
+</script>
+<!-- //script for responsive tabs -->   
+<!-- stats -->
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.countup.js"></script>
+  <script>
+    $('.counter').countUp();
+  </script>
+<!-- //stats -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+<script type="text/javascript">
+  jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){   
+      event.preventDefault();
+      $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+    });
+  });
+</script>
+<!-- here stars scrolling icon -->
+  <script type="text/javascript">
+    $(document).ready(function() {
+      /*
+        var defaults = {
+        containerID: 'toTop', // fading element id
+        containerHoverID: 'toTopHover', // fading element hover id
+        scrollSpeed: 1200,
+        easingType: 'linear' 
+        };
+      */
+                
+      $().UItoTop({ easingType: 'easeOutQuart' });
+                
+      });
+  </script>
+<!-- //here ends scrolling icon -->
+
+
+<!-- for bootstrap working -->
+<script type="text/javascript" src="js/bootstrap.js"></script>
 
 </body>
 </html>
